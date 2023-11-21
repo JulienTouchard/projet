e25c57d76e606bdc5a2072a0bda082a7d3130351
JulienTouchard <julien.touchard@free.fr>
2023-11-21 15:30:24 +0100

    first commit

---
+++ src/components/ListMenu/ListMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ListMenu.css';
+
+const ListMenu = () => (
+  <div className="ListMenu">
+    ListMenu Component
+  </div>
+);
+
+
+
+export default ListMenu;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,11 @@
+import ListMenu from '../ListMenu/ListMenu';
+import "./Menu.css";
+const Menu = ()=>{
+    return(
+        <nav>Hello wordl
+
+            <ListMenu></ListMenu>
+        </nav>
+    )
+}
+export default Menu;
